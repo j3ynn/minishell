ea91101dd15434bc6784719f4db908e200b4317b
Jenny <jennybellucci43@gmail.com>
2025-09-01 14:53:31 +0200

    tokenizer quasi completato, inizio parser

---
+++ tokanize/tokanize_base.c
@@ -0,0 +1,107 @@
+#include "../minishell.h"
+
+int	handle_quotes(char *str)
+{
+	char	quote;
+
+	int i = 0;
+	quote = str[i];
+	(i)++;
+	while (str[i])
+	{
+		if (str[i] == quote)
+		{
+			(i)++;
+			return (1);
+		}
+		(i)++;
+	}
+	return (0);
+}
+
+int	skip_whitespace(char *str)
+{
+	int i = 0;
+
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	return (i);
+}
+
+char	**add_token(char **tokens, const char *token)
+{
+	int		i = 0;
+	int		j = 0;
+	char	**new_tokens;
+
+	while (tokens[i])
+		i ++;
+	new_tokens = malloc(sizeof(char *) * (i + 2));
+	if (!new_tokens)
+	{
+		free_tokens(tokens);
+		return (NULL);
+	}
+	while (j < i)
+	{
+		new_tokens[j] = tokens[j];
+		j ++;
+	}
+	new_tokens[i] = ft_strdup(token);
+	new_tokens[i + 1] = NULL;
+	free_tokens(tokens);
+	return (new_tokens);
+}
+
+char	*get_word(char *str, int start)
+{
+	int		len;
+	int		i;
+	char	*word;
+
+	len = get_manage(str, start);
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = str[start + i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**process_input(char *input)
+{
+	char	**tokens;
+	char	*word;
+	int		i;
+
+	tokens = malloc(sizeof(char *) * 1);
+	if (!tokens)
+		return (NULL);
+	tokens[0] = NULL;
+	i = 0;
+	while (input[i])
+	{
+		i += skip_whitespace(input + i);
+		if (!input[i])
+			break;
+		word = get_word(input, i);
+		if (!word)
+		{
+			free_tokens(tokens);
+			return (NULL);
+		}
+		tokens = add_token(tokens, word);
+		free(word);
+		if (!tokens)
+			return (NULL);
+		i += get_manage(input, i);
+	}
+	return (tokens);
+}
+
+
---
+++ tokanize/tokenize_utils.c
@@ -0,0 +1,56 @@
+#include "../minishell.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	get_quoted_len(char *str, int start)
+{
+	char	quote;
+	int		i;
+
+	quote = str[start];
+	i = start + 1;
+	while (str[i] && str[i] != quote)
+		i++;
+	if (str[i] == quote)
+		i++;
+	return (i - start);
+}
+
+int	get_operator_len(char *str, int start)
+{
+	if ((str[start] == '>' && str[start + 1] == '>') ||
+		(str[start] == '<' && str[start + 1] == '<'))
+		return (2);
+	return (1);
+}
+
+int	get_normal_len(char *str, int start)
+{
+	int	i;
+
+	i = start;
+	while (str[i] && str[i] != ' ' && str[i] != '\t' &&
+		   str[i] != '|' && str[i] != '<' && str[i] != '>' &&
+		   str[i] != '\'' && str[i] != '"')
+		i++;
+	return (i - start);
+}
+
+int	get_manage(char *str, int start)
+{
+	if (str[start] == '\'' || str[start] == '"')
+		return (get_quoted_len(str, start));
+	else if (str[start] == '|' || str[start] == '<' || str[start] == '>')
+		return (get_operator_len(str, start));
+	else
+		return (get_normal_len(str, start));
+}
+
